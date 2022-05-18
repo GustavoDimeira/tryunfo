84ff6fee83a858130bef424bb48281afd13ea437
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-05-18 14:35:49 -0400

    2

--- src/App.js
+++ src/App.js
@@ -3,10 +3,31 @@ import Form from './components/Form';
 
 class App extends React.Component {
   render() {
+    const handleInputChange = () => 'a';
+    const handleSaveButtonClick = () => 'a';
+    const soma = 1 + 1;
+    const temp = () => {
+      if (soma) {
+        return true;
+      }
+    };
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form />
+        <Form
+          cardName=""
+          cardDescription=""
+          cardAttr1=""
+          cardAttr2=""
+          cardAttr3=""
+          cardImage=""
+          cardRare=""
+          cardTrunfo={ temp() }
+          hasTrunfo={ temp() }
+          isSaveButtonDisabled={ temp() }
+          onInputChange={ handleInputChange }
+          onSaveButtonClick={ handleSaveButtonClick }
+        />
       </div>
     );
   }
