c8646fb5e7eaeda3311b707405b479602e2f226a
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-05-18 10:56:29 -0400

    resolendo lint

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,19 +4,19 @@ class Form extends React.Component {
   render() {
     return (
       <form>
-        <input type="text" data-testid="name-input"></input>
-        <input type="textArea" data-testid="description-input"></input>
-        <input type="number" data-testid="attr1-input"></input>
-        <input type="number" data-testid="attr2-input"></input>
-        <input type="number" data-testid="attr3-input"></input>
-        <input type="text" data-testid="image-input"></input>
+        <input type="text" data-testid="name-input" />
+        <input type="textArea" data-testid="description-input" />
+        <input type="number" data-testid="attr1-input" />
+        <input type="number" data-testid="attr2-input" />
+        <input type="number" data-testid="attr3-input" />
+        <input type="text" data-testid="image-input" />
         <select data-testid="rare-input">
           <option>normal</option>
           <option>raro</option>
           <option>muito raro</option>
         </select>
-        <input type="checkbox" data-testid="trunfo-input"></input>
-        <button data-testid="save-button">Salvar</button>
+        <input type="checkbox" data-testid="trunfo-input" />
+        <button type="submit" data-testid="save-button">Salvar</button>
       </form>
     );
   }
